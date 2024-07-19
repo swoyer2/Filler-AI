db8e32c0f877ed15c00ce57805f8db0bfea28de3
Shawn Xu <xu107288696@gmail.com>
2024-07-19 16:22:25 -0700

    Cleanup

--- board.py
+++ board.py
@@ -2,48 +2,47 @@ from random import choice
 import copy
 
 class Board:
-    def __init__(self, size):
-        self.size = size
-        self.board = [[None] * (size) for _ in range(size-1)]
-        self.colors = None
-    
-    def printBoard(self):
+    def __init__(self, size: int) -> None:
+        self.size: int = size
+        self.board: list[list[int]] = [[-1] * (size) for _ in range(size-1)]
+        self.colors: list[int] | None = None
+
+    def printBoard(self) -> None:
         for row in self.board:
             print(row)
         print('\n')
-    
-    def getBoard(self):
+
+    def getBoard(self) -> list[list[int]]:
         return self.board
-    
-    def setColor(self, pos):
+
+    def setColor(self, pos: tuple[int, int]) -> None:
         if pos == (6, 0):
-            try:
-                toRemove = self.checkAvailable(pos).remove(self.board[0][7])
-                self.board[pos[0]][pos[1]] = choice(toRemove)
-            except:
-                self.board[pos[0]][pos[1]] = choice(self.checkAvailable(pos))
+           avaliableColors: list[int] = self.checkAvailable(pos)
+           if self.board[0][7] in avaliableColors:
+               avaliableColors.remove(self.board[0][7])
+           self.board[pos[0]][pos[1]] = choice(avaliableColors)
         else:
             self.board[pos[0]][pos[1]] = choice(self.checkAvailable(pos))
-    
-    def checkAvailable(self, pos):
-        availableColors = [0, 1, 2, 3, 4, 5]
-        colorsToRemove = set()
+
+    def checkAvailable(self, pos : tuple[int, int]) -> list[int]:
+        availableColors: list[int] = [0, 1, 2, 3, 4, 5]
+        colorsToRemove: set[int] = set()
         # Check the cell to the left
-        if pos[0] > 0 and self.board[pos[0] - 1][pos[1]] is not None:
+        if pos[0] > 0:
             colorsToRemove.add(self.board[pos[0] - 1][pos[1]])
-        
+
         # Check the cell above
-        if pos[1] > 0 and self.board[pos[0]][pos[1] - 1] is not None:
+        if pos[1] > 0:
             colorsToRemove.add(self.board[pos[0]][pos[1] - 1])
 
         output = [ele for ele in availableColors if ele not in colorsToRemove]
-        
+
         return output
-    
-    def buildBoard(self):
+
+    def buildBoard(self) -> None:
         for y in range(self.size):
             for x in range(self.size-1):
                 self.setColor((x,y))
 
-    def getBoardCopy(self):
+    def getBoardCopy(self) -> list[list[int]]:
         return copy.deepcopy(self.board)
--- minmax.py
+++ minmax.py
@@ -1,13 +1,14 @@
 from board import Board
 from anytree import Node as AnyNode
+from typing import Any
 import cProfile
 
 
 class MinMax:
-    def __init__(self, gameBoard) -> None:
-        self.gameBoard = gameBoard
+    def __init__(self, gameBoard : Board) -> None:
+        self.gameBoard : Board = gameBoard
 
-    def getAvailableMoves(self, color1, color2):
+    def getAvailableMoves(self, color1 : int, color2 : int) -> list[int]:
         moves = [0, 1, 2, 3, 4, 5]
         moves.remove(color1)
         # Sometimes the board generates with both players starting the same color
@@ -18,21 +19,21 @@ class MinMax:
         return moves
 
     # Checks each neighbor of the player starting from the corner, if same color then it gets added to the list
-    def findPlayerPositions(self, board):
-        player1 = set([(0, 7)])
-        player2 = set([(6, 0)])
+    def findPlayerPositions(self, board : list[list]) -> tuple[list[tuple[int, int]], list[tuple[int, int]]]:
+        player1: tuple[int, int] = (0, 7)
+        player2: tuple[int, int] = (6, 0)
 
-        def helper(playerPositions):
-            toCheck = set(playerPositions)
-            new_positions = set(playerPositions)
-            visited = set()
+        def helper(playerPosition : tuple[int, int]) -> list[tuple[int, int]]:
+            toCheck: set[tuple[int, int]] = {playerPosition}
+            new_positions: set[tuple[int, int]] = {playerPosition}
+            visited: set[tuple[int, int]] = set()
 
-            neighbors = {
+            neighbors: set[tuple[int, int]] = {
                 (-1, 0), (1, 0), (0, -1), (0, 1)
             }
 
             while new_positions:
-                next_positions = set()
+                next_positions: set[tuple[int, int]] = set()
                 for pos in new_positions:
                     for dx, dy in neighbors:
                         neighbor = (pos[0] + dx, pos[1] + dy)
@@ -48,7 +49,7 @@ class MinMax:
         return helper(player1), helper(player2)
 
     # This will convery all player positions to the color selected
-    def simulate(self, colors, player1, origBoard):
+    def simulate(self, colors, player1 : bool, origBoard : Board) -> int:
         board = origBoard.getBoardCopy()
         for color in colors[2:]:
             positions = self.findPlayerPositions(board)
@@ -62,7 +63,7 @@ class MinMax:
                     board[pos[0]][pos[1]] = color
 
         # If position is winning give it a greater value (Or lesser if its the opponent)
-        positions = self.findPlayerPositions(board)
+        positions: tuple[list[tuple[int, int]], list[tuple[int, int]]] = self.findPlayerPositions(board)
         if len(positions[0]) > 28:
             return 100 + len(positions[0])
         elif len(positions[1]) > 28:
@@ -71,19 +72,19 @@ class MinMax:
         score = len(positions[0]) - len(positions[1])
         return score
 
-    def generateTree(self, board, maxDepth, depth, a, b, maximizing_player):
+    def generateTree(self, board: AnyNode, maxDepth : int, depth : int, a: int | float, b: int | float, maximizing_player: bool) -> tuple[AnyNode | None, int | float, Any]:
         if depth == maxDepth:
             return None, self.simulate(board.colors, maximizing_player, self.gameBoard), [board.colors]
 
         best_path = None
-        best_eval = float('-inf') if maximizing_player else float('inf')
+        best_eval: int | float = float('-inf') if maximizing_player else float('inf')
         best_path_colors = None
 
         # Branch off for each of the four moves
         for move in self.getAvailableMoves(board.colors[-2], board.colors[-1]):
             newColors = board.colors.copy()
             newColors.append(move)
-            child_node = AnyNode(name=f"{board.name}{move}", parent=board, colors=newColors)
+            child_node: AnyNode = AnyNode(name=f"{board.name}{move}", parent=board, colors=newColors)
 
             # Evaluate the child node using Minimax
             _, eval, path = self.generateTree(child_node, maxDepth, depth + 1, a, b, not maximizing_player)
@@ -106,8 +107,8 @@ class MinMax:
 
         return best_path, best_eval, best_path_colors
 
-    def evaluate_tree(self, board, maxDepth, isPlayer1):
-        root = AnyNode(name="Root", colors=[board.board[6][0], board.board[0][7]])
+    def evaluate_tree(self, board : Board, maxDepth : int, isPlayer1):
+        root: AnyNode = AnyNode(name="Root", colors=[board.board[6][0], board.board[0][7]])
         best_path, best_score, best_path_colors = self.generateTree(root, maxDepth, 0, float('-inf'), float('inf'), isPlayer1)
         return best_path_colors, best_score
 
